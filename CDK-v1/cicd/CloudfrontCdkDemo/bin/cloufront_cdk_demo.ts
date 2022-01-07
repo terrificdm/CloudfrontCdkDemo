08aabf4d9b84c0f562783da3ccab0332daa3aec0
terrificdm <62762145+terrificdm@users.noreply.github.com>
2022-01-07 15:17:16 +0800

    Update cloufront_cdk_demo.ts

--- CDK-v1/cicd/CloudfrontCdkDemo/bin/cloufront_cdk_demo.ts
+++ CDK-v1/cicd/CloudfrontCdkDemo/bin/cloufront_cdk_demo.ts
@@ -10,9 +10,9 @@ new InfraCdkStack(app, 'InfraCdkStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
 
-new CloudfrontCdkStack(app, 'CloudfrontCdkStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
+// new CloudfrontCdkStack(app, 'CloudfrontCdkStack', {
+//   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+// });
 
 new PipelineStack(app, 'PipelineStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
